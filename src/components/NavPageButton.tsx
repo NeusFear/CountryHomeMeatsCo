ba9445505081de3f3437b1b951722e134c81c25d
Wyn Price <wynprice999@gmail.com>
2021-01-18 01:07:18 +0000

    Fixed icon stuff

--- src/components/NavPageButton.tsx
+++ src/components/NavPageButton.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react'
-
 type Props = {
   page: string;
   icon: string;
 }
 
 export const NavPageButton = (props: Props) => {
+  import Img from `../assets/icons/${props.icon}.svg`
   return (
     <div className="flex items-center space-x-3 text-gray-700 p-2 rounded-md font-medium hover:bg-gray-200 bg-gray-200 focus:shadow-outline">
-      <img className="text-gray-600 w-8 h-8 bg-gray-700 hover:bg-gray-800" src={require(`./assets/icons/${props.icon}.svg`)} />
+      <Img className="text-gray-600 w-8 h-8 bg-gray-700 hover:bg-gray-800" />
       <span>{props.page}</span>
     </div>
   )

0b1bb8185e659eaa49c0993ab51cd5e7952d4607
Wyn Price <wynprice999@gmail.com>
2021-01-18 01:23:06 +0000

    Fixed icon

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,9 @@ import { UsersPage } from './pages/UsersPage';
 import { CalendarPage } from './pages/CalendarPage';
 import { TodayPage } from './pages/TodayPage';
 import { NavPageButton } from './components/NavPageButton';
+
+import CalendarSvg from './assets/icons/calendar.svg'
+
 import './styles/navbar.css'
 import './styles/tailwind.scss'
 
@@ -43,7 +46,7 @@ export const App = (props: any) => {
             <li><Link to="/">Home</Link></li>
             <li><Link to="/users">Users</Link></li>
             <li><Link to="/today">Today</Link></li>
-            <li><Link to="/calendar"><NavPageButton page="Calendar" icon="calendar" /></Link></li>
+            <li><Link to="/calendar"><NavPageButton Page="Calendar" Icon={CalendarSvg} /></Link></li>
           </ul>
 
           <Switch>
--- src/components/NavPageButton.tsx
+++ src/components/NavPageButton.tsx
@@ -1,15 +1,10 @@
 import * as React from 'react'
-type Props = {
-  page: string;
-  icon: string;
-}
 
-export const NavPageButton = (props: Props) => {
-  import Img from `../assets/icons/${props.icon}.svg`
+export const NavPageButton = ({ Page, Icon }) => {
   return (
     <div className="flex items-center space-x-3 text-gray-700 p-2 rounded-md font-medium hover:bg-gray-200 bg-gray-200 focus:shadow-outline">
-      <Img className="text-gray-600 w-8 h-8 bg-gray-700 hover:bg-gray-800" />
-      <span>{props.page}</span>
+      <Icon className="text-gray-600 w-8 h-8 bg-gray-700 hover:bg-gray-800" />
+      <span>{page}</span>
     </div>
   )
 }

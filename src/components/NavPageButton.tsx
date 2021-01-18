7915ecd1ab867cb68f07904cd8c526922f9644a1
Brandon Davis <neusfear@gmail.com>
2021-01-17 19:41:52 -0600

    p -> P

--- src/components/NavPageButton.tsx
+++ src/components/NavPageButton.tsx
@@ -4,7 +4,7 @@ export const NavPageButton = ({ Page, Icon }) => {
   return (
     <div className="flex items-center space-x-3 text-gray-700 p-2 rounded-md font-medium hover:bg-gray-200 bg-gray-200 focus:shadow-outline">
       <Icon className="text-gray-600 w-8 h-8 bg-gray-700 hover:bg-gray-800" />
-      <span>{page}</span>
+      <span>{Page}</span>
     </div>
   )
 }

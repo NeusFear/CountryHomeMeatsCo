723f8e6d96ec603660a50490b54fae121dd57c9e
Brandon Davis <neusfear@gmail.com>
2021-01-19 16:41:49 -0600

    fix today icon colors

--- src/components/NavPageButton.tsx
+++ src/components/NavPageButton.tsx
@@ -3,7 +3,7 @@ import * as React from 'react'
 export const NavPageButton = ({ Page, Icon }) => {
   return (
     <div className="group bg-gray-50 shadow-sm hover:shadow-lg hover:border-transparent p-1 ml-1 mr-2 my-2 rounded-lg">
-      <Icon className="text-gray-800 group-hover:text-gray-900 w-5 h-5 float-left mr-2 mt-0.5" />
+      <Icon className="text-gray-800 group-hover:text-tomato-900 w-5 h-5 float-left mr-2 mt-0.5" />
       <p className="text-gray-800 group-hover:text-gray-900">{Page}</p>
     </div>
   )

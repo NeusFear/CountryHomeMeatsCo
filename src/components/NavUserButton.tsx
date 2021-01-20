36da7b594591b4329edee13cae37f9c2542529d3
Wyn Price <wynprice999@gmail.com>
2021-01-20 18:42:51 +0000

    Replace x with a cross

--- src/components/NavUserButton.tsx
+++ src/components/NavUserButton.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import * as DummyDatabase from "../DummyDatabase"
+import { SvgCross } from "../assets/Icons"
 
 export const NavUserButton = ({id, onClick, onRemove, selected, Icon} : {id: number, onClick: any, onRemove: any, selected: boolean, Icon: any}) => {
     let details = DummyDatabase.getUserById(id)
@@ -7,7 +8,7 @@ export const NavUserButton = ({id, onClick, onRemove, selected, Icon} : {id: num
       <div className={"group bg-gray-200 shadow-sm hover:shadow-lg p-1 ml-1 mr-2 my-2 rounded-lg flex flex-row" + (selected?" bg-tomato-400" : "")} onClick={onClick}>
         <Icon className="text-gray-800 group-hover:text-tomato-900 w-8 h-8 float-left" />
         <span className="text-gray-800 group-hover:text-gray-900 flex-grow ml-4">{details.name}</span>
-        <span className="text-gray-800 group-hover:text-gray-900 float-right" onClick={e => { onRemove(); e.stopPropagation() }} >X</span>
+        <span className="text-gray-800 hover:text-tomato-900 float-right flex items-center" onClick={e => { onRemove(); e.stopPropagation() }} ><SvgCross /></span>
       </div>
     )
   }

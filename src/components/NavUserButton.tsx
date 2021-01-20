9ef7880cd466fc39f59a7f394ea854e5a6c3ed90
Brandon Davis <neusfear@gmail.com>
2021-01-20 12:44:37 -0600

    try to add icon to nav user button

--- src/components/NavUserButton.tsx
+++ src/components/NavUserButton.tsx
@@ -6,7 +6,7 @@ export const NavUserButton = ({id, onClick, onRemove, selected, Icon} : {id: num
     let details = DummyDatabase.getUserById(id)
     return (
       <div className={"group bg-gray-200 shadow-sm hover:shadow-lg p-1 ml-1 mr-2 my-2 rounded-lg flex flex-row" + (selected?" bg-tomato-400" : "")} onClick={onClick}>
-        <Icon className="text-gray-800 group-hover:text-tomato-900 w-8 h-8 float-left" />
+        <Icon className="text-gray-800 group-hover:text-tomato-900 w-4 h-4 float-left" />
         <span className="text-gray-800 group-hover:text-gray-900 flex-grow ml-4">{details.name}</span>
         <span className="text-gray-800 hover:text-tomato-900 float-right flex items-center" onClick={e => { onRemove(); e.stopPropagation() }} ><SvgCross /></span>
       </div>

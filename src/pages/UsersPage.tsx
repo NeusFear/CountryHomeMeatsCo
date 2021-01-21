6e61a9473e0899bc216260541b2f976289586149
Wyn Price <wynprice999@gmail.com>
2021-01-21 22:05:44 +0000

    Fix spacing on user page

--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -32,17 +32,17 @@ export const UsersPage = ({ pinnedList }: { pinnedList: UserPinnedList }) => {
   }
 
   return (<>
-    <div className="bg-gray-800 py-2 px-4 flex flex-row">
-      <div className="relative rounded-md shadow-sm w-11/12">
+    <div className="bg-gray-800 py-2 px-2 flex flex-row justify-end">
+      <div className="relative rounded-md shadow-sm flex-grow">
         <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
           <span className="text-gray-500 sm:text-sm">
             <SvgSearch />
           </span>
         </div>
-        <input type="text" name="search" id="search" className="block w-5/6 pl-9 pr-12 border-gray-300 rounded-md h-10" placeholder="Search" />
+        <input type="text" name="search" id="search" className="block w-full pl-9 pr-12 border-gray-300 rounded-md h-10" placeholder="Search" />
       </div>
-      <div className="transform -translate-x-64 cursor-pointer px-4 w-12 pt-3 hover:bg-tomato-600 border-gray-300 rounded-md h-10 flex-initial bg-tomato-700 text-white"><SvgSearch /></div>
-      <div className="transform -translate-x-60 cursor-pointer px-4 w-12 pt-3 hover:bg-tomato-600 border-gray-300 rounded-md h-10 flex-initial bg-tomato-700 text-white" onClick={() => history.push(editUserDetailsPage, /*In production, don't have this*/createDebugUserDetails())}><SvgNewUser /></div>
+      <div className="transform cursor-pointer px-4 w-12 ml-1 pt-3 hover:bg-tomato-600 border-gray-300 rounded-md h-10 flex-initial bg-tomato-700 text-white"><SvgSearch /></div>
+      <div className="transform cursor-pointer px-4 w-12 ml-1 pt-3 hover:bg-tomato-600 border-gray-300 rounded-md h-10 flex-initial bg-tomato-700 text-white" onClick={() => history.push(editUserDetailsPage, /*In production, don't have this*/createDebugUserDetails())}><SvgNewUser /></div>
     </div>
     <div className="bg-gray-400 px-1 py-0.5 shadow-sm flex flex-row mb-2">
       <span className="ml-6 pr-2 flex-1 text-gray-700">Name<span className="text-gray-600 ml-1">#ID</span></span>

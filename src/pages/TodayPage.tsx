c1798dcbe4f14a40e14307028ad8c58a21ee9f2d
Brandon Davis <neusfear@gmail.com>
2021-01-26 15:13:13 -0600

    add link to animal page from today

--- src/pages/TodayPage.tsx
+++ src/pages/TodayPage.tsx
@@ -8,7 +8,7 @@ import User, { IUser, useUsers } from "../database/types/User"
 import { editAnimalEaters, scheduleAnimal, setModal } from "../modals/ModalManager"
 import { SchueduleAnimalModal } from "../modals/ScheduleAnimalModal"
 import { getDayNumber } from "../Util"
-import { userDetailsPage } from "../NavBar";
+import { userDetailsPage, animalDetailsPage } from "../NavBar";
 
 export const TodayPage = () => {
   const today = React.useMemo(() => new Date(), [])
@@ -106,9 +106,11 @@ const SlaughterInfo = ({animal}: {animal: IAnimal}) => {
   if(user === undefined) {
     return (<div>Loading...</div>)
   }
+
+  const history = useHistory();
   
   return (
-    <div className="group bg-gray-100 shadow-sm hover:shadow-lg hover:border-transparent p-1 mx-4 mt-1 my-2 rounded-lg flex flex-row" onClick={() => console.log("go to animal")}>
+    <div className="group bg-gray-100 shadow-sm hover:shadow-lg hover:border-transparent p-1 mx-4 mt-1 my-2 rounded-lg flex flex-row" onClick={() => history.push(animalDetailsPage, animal.id)}>
       <ReactTooltip delayShow={200} /> 
       <div className="w-20">
         <SvgCow className="text-gray-800 group-hover:text-tomato-900 w-5 h-5 mr-2 mt-0.5 ml-4 transform translate-y-1/2" />
@@ -130,7 +132,7 @@ const UserTag = ({user, id} : {user: IUser, id?: number}) => {
   const history = useHistory();
   return (
     <div className="flex">
-      <p className="bg-gray-200 px-2 py-1 rounded-lg text-sm mt-0.5 cursor-pointer hover:bg-gray-300" onClick={() => history.push(userDetailsPage, user.id)}>{user.name}</p>
+      <p className="bg-gray-200 px-2 py-1 rounded-lg text-sm mt-0.5 cursor-pointer hover:bg-gray-300" onClick={e => {history.push(userDetailsPage, user.id); e.stopPropagation();}}>{user.name}</p>
       {id && <p className="bg-gray-200 px-2 py-1 rounded-lg text-sm mt-0.5 cursor-pointer hover:bg-gray-300" onClick={() => console.log("go to sub user's individual cut instructions")}>#{id}</p>}
     </div>
   )

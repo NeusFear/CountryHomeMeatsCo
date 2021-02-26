c0a306891d1cc5efe15282a7e40ad6d10bc268bc
Wyn Price <wynprice999@gmail.com>
2021-02-26 01:42:59 +0000

    Bruh

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -53,8 +53,8 @@ export const NavBar = ({ pinnedList }: { pinnedList: UserPinnedList }) => {
   return (
     <div className="bg-gray-100 w-6/12 flex flex-col h-full md:w-4/14 lg:w-2/12">
       <div className="bg-tomato-800 h-14 flex flex-row">
-        <div className="m-2" onClick={history.goBack()}><SvgArrow className="hover:text-tomato-800 hover:shadow-md cursor-pointer text-4xl w-10 h-10 text-tomato-500 bg-white rounded-full p-0.5 transform rotate-180" /></div>
-        <div className="m-2" onClick={history.goForward()}><SvgArrow className="hover:text-tomato-800 hover:shadow-md cursor-pointer text-4xl w-10 h-10 text-tomato-500 bg-white rounded-full p-0.5" /></div>
+        <div className="m-2" onClick={() => history.goBack()}><SvgArrow className="hover:text-tomato-800 hover:shadow-md cursor-pointer text-4xl w-10 h-10 text-tomato-500 bg-white rounded-full p-0.5 transform rotate-180" /></div>
+        <div className="m-2" onClick={() => history.goForward()}><SvgArrow className="hover:text-tomato-800 hover:shadow-md cursor-pointer text-4xl w-10 h-10 text-tomato-500 bg-white rounded-full p-0.5" /></div>
       </div>
       <ul>
         <li><Link to="/"><NavPageButton Page="Home" Icon={SvgHome} /></Link></li>

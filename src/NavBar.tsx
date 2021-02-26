1b38d6f14910150ed05fe7a5517f35c1ec4dad21
Wyn Price <wynprice999@gmail.com>
2021-02-26 01:52:09 +0000

    highlighting when can go forward and backwards

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -15,6 +15,7 @@ import { useHistoryListState } from "./AppHooks";
 import { SvgCalendar, SvgHome, SvgDay, SvgUsers, SvgArrow } from './assets/Icons';
 import { UserPinnedList } from './App';
 import { NavPinnedUserEntry } from './components/NavPinnedUserEntry';
+import { useEffect, useState } from 'react';
 
 const logo = require('./assets/logo.png');
 
@@ -45,6 +46,7 @@ export const NavBar = ({ pinnedList }: { pinnedList: UserPinnedList }) => {
   const allPinned = pinnedList.allPinned
   const history = useHistory()
   const selectedId = useHistoryListState()
+  const fb = useHistoryForwardBackwards()
   const onDetailsClicked = (id: string) => {
     //Select the user details page with the id as the state
     history.push(userDetailsPage, id)
@@ -53,8 +55,8 @@ export const NavBar = ({ pinnedList }: { pinnedList: UserPinnedList }) => {
   return (
     <div className="bg-gray-100 w-6/12 flex flex-col h-full md:w-4/14 lg:w-2/12">
       <div className="bg-tomato-800 h-14 flex flex-row">
-        <div className="m-2" onClick={() => history.goBack()}><SvgArrow className="hover:text-tomato-800 hover:shadow-md cursor-pointer text-4xl w-10 h-10 text-tomato-500 bg-white rounded-full p-0.5 transform rotate-180" /></div>
-        <div className="m-2" onClick={() => history.goForward()}><SvgArrow className="hover:text-tomato-800 hover:shadow-md cursor-pointer text-4xl w-10 h-10 text-tomato-500 bg-white rounded-full p-0.5" /></div>
+        <div className="m-2" onClick={() => fb.back?history.go(-1):false}><SvgArrow className={`hover:text-${fb.back?"tomato":"gray"}-800 text-${fb.back?"tomato":"gray"}-500 hover:shadow-md cursor-pointer text-4xl w-10 h-10 bg-white rounded-full p-0.5 transform rotate-180`} /></div>
+        <div className="m-2" onClick={() => fb.forward?history.go(1):false}><SvgArrow className={`hover:text-${fb.forward?"tomato":"gray"}-800 text-${fb.forward?"tomato":"gray"}-500 hover:shadow-md cursor-pointer text-4xl w-10 h-10 bg-white rounded-full p-0.5`} /></div>
       </div>
       <ul>
         <li><Link to="/"><NavPageButton Page="Home" Icon={SvgHome} /></Link></li>
@@ -79,4 +81,15 @@ export const NavBar = ({ pinnedList }: { pinnedList: UserPinnedList }) => {
       </div>
     </div>
   )
+}
+
+const useHistoryForwardBackwards: () => { 
+  forward: boolean,
+  back: boolean
+} = () => {
+  const history = useHistory()
+  const create = () => { return {forward: history.canGo(1), back: history.canGo(-1)} }
+  const [state, setState] = useState(create())
+  useEffect(() => history.listen(() => setState(create())))
+  return state
 }

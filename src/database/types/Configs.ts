639f020787f36b54dd1c5b1aad6a60960aad2915
Wyn Price <wynprice999@gmail.com>
2021-03-02 02:27:28 +0000

    Added editable price sheet

--- src/database/types/Configs.ts
+++ src/database/types/Configs.ts
@@ -1,39 +1,96 @@
 import { createResultWatcher } from './../Database';
 import mongoose, { Schema, Document } from 'mongoose';
 
-const Configs = ["FullDays"] as const
+const Configs = ["FullDays", "PriceData"] as const
+
+export interface PriceData extends Document {
+  currentPrices: {
+    beef: {
+      slaughter: number
+      processing: number
+      patties: number
+      halves: number
+      halvesToQuaters: number
+      extraBoning: number
+      cutStewMeat: number
+      boneAndTenderizeRoundSteaks: number
+      makeCubedSteaks: number
+      boneOrPrimeRib: number
+      boneOutLoin: number
+    },
+    pork: {
+      slaughter: number,
+      slaughter150lb: number,
+      processing: number,
+      cure: number,
+      sausage: number
+    }
+  }
+}
 
 export interface IFullDaysConfig extends Document {
-  configType: typeof Configs[0],
   dates: Date[]
 }
 
 type ConfigTypes<T> = 
   T extends typeof Configs[0] ? IFullDaysConfig :
+  T extends typeof Configs[1] ? PriceData :
   never;
 
-type AllConfigs = IFullDaysConfig
+type AllConfigs = IFullDaysConfig & PriceData
 
 const configSchema = new Schema({
-  configType: { type: String, enum: Configs },
+  //Full days:
+  dates: { type: [Schema.Types.Date], required: true },
 
-  dates: { type: [Schema.Types.Date] }
+  //Price: data
+  currentPrices: { type: {
+    beef: { type: {
+      slaughter: { type: Number, required: true },
+      processing: { type: Number, required: true },
+      patties: { type: Number, required: true },
+      halves: { type: Number, required: true },
+      halvesToQuaters: { type: Number, required: true },
+      extraBoning: { type: Number, required: true },
+      cutStewMeat: { type: Number, required: true },
+      boneAndTenderizeRoundSteaks: { type: Number, required: true },
+      makeCubedSteaks: { type: Number, required: true },
+      boneOrPrimeRib: { type: Number, required: true },
+      boneOutLoin: { type: Number, required: true },
+    }, required: true },
+    pork: { type: {
+      slaughter: { type: Number, required: true },
+      slaughter150lb: { type: Number, required: true },
+      processing: { type: Number, required: true },
+      cure: { type: Number, required: true },
+      sausage: { type: Number, required: true },
+    }, required: true }
+  }, required: true },
+  
 })
 
 const ConfigModel = mongoose.model<AllConfigs>('Configs', configSchema)
 
 const useConfigs = createResultWatcher(ConfigModel)
 
-export const useConfig = <T extends typeof Configs[number]>(configType: T): ConfigTypes<T> =>  {
+export const useConfig = <T extends typeof Configs[number]>(_: T): ConfigTypes<T> =>  {
   const configs = useConfigs(ConfigModel.find())
   if(configs === undefined) {
     return undefined
   }
   if(configs.length === 0) {
     return new ConfigModel({
-      configType: configType as string,
-      dates: []
-    } as ConfigTypes<T>) as ConfigTypes<T>
+      dates: [],
+
+      currentPrices: {
+        beef: {
+          slaughter: 75, processing: 1, patties: 0.45, halves: 3, halvesToQuaters: 5,
+          extraBoning: 0.2, cutStewMeat: 0.5, boneAndTenderizeRoundSteaks: 3, 
+          makeCubedSteaks: 3, boneOrPrimeRib: 5, boneOutLoin: 10
+        },
+        pork: { slaughter: 50, slaughter150lb: 60, processing: 1, cure: 1, sausage: 1 }
+      }
+    } as AllConfigs) as ConfigTypes<T>
   }
   return configs[0] as ConfigTypes<T>
 }
--- src/pages/PriceSheet.tsx
+++ src/pages/PriceSheet.tsx
@@ -1,12 +1,36 @@
+import { createContext, useContext, useRef, useState } from "react"
+import { useConfig } from "../database/types/Configs"
+
+const GlobalEdit = createContext(false)
+
 export const PriceSheet = () => {
-  
-    return (
+  const priceData = useConfig('PriceData')
+  const [globalEdit, setGlobalEdit] = useState(false)
+
+  const thenSave = (setter: (val: number) => void) => {
+    return (val: number) => {
+      setter(val)
+      //To check if a object as changed, mongoose does == on the object,
+      //And == on it's children. As we have 3 inner objects, we need to mark the field
+      //as changed. This is slow and can be optimised.
+      priceData.markModified('currentPrices')
+      priceData.save()
+    }
+  }
+  if(priceData === undefined) {
+    return (<div>Loading Price List...</div>)
+  }
+  const { beef, pork } = priceData.currentPrices
+
+  return (
     <div className="w-full h-screen flex flex-col">
+      <GlobalEdit.Provider value={globalEdit}>
         <div className="flex flex-row w-full h-14 bg-gray-800 pt-1">
           <div className="text-white text-4xl font-bold ml-4">PRICE SHEET</div>
         </div>
-        <div className="bg-gray-400 px-1 py-0.5 shadow-sm mb-2">
-          <span className="ml-2 text-gray-700">This page's prices are used to calculate processing invoices</span>
+        <div className="bg-gray-400 px-1 py-0.5 shadow-sm mb-2 flex flex-row">
+          <div className="ml-2 text-gray-700 flex-grow">This page's prices are used to calculate processing invoices</div>
+          <div className="mr-2 text-gray-700 bg-tomato-500" onClick={() => setGlobalEdit(!globalEdit)}>Edit All</div>
         </div>
         <div className="px-4 mt-4 h-full overflow-y-scroll">
 
@@ -19,17 +43,17 @@ export const PriceSheet = () => {
               </tr>
             </thead>
             <tbody className="border-b border-tomato-600">
-              <PriceEntry description="Slaughter Fee" currentPrice={75.00} unit="per Animal"/>
-              <PriceEntry description="Processing Fee" currentPrice={1.00} unit="per Pound * Dressed Weight"/>
-              <PriceEntry description="Make Patties" currentPrice={0.45} unit="per Pound"/>
-              <PriceEntry description="Split into Halves" currentPrice={3.00} unit="per Half"/>
-              <PriceEntry description="Divide Halves into Quaters" currentPrice={5.00} unit="per Quarter"/>
-              <PriceEntry description="Extra Boning" currentPrice={0.20} unit="per Quarter"/>
-              <PriceEntry description="Cut Stew Meat" currentPrice={0.50} unit="per Pound"/>
-              <PriceEntry description="Bone and Tenderize Round Steaks" currentPrice={3.00} unit="per Pound"/>
-              <PriceEntry description="Make Cubed Steaks" currentPrice={3.00} unit="per Half"/>
-              <PriceEntry description="Bone Our Prime Rib" currentPrice={5.00} unit="per Half"/>
-              <PriceEntry description="Bone out Loin" currentPrice={10.00} unit="per Half"/>
+              <PriceEntry description="Slaughter Fee" currentPrice={beef.slaughter} onChange={thenSave(v => beef.slaughter = v)} unit="per Animal"/>
+              <PriceEntry description="Processing Fee" currentPrice={beef.processing} onChange={thenSave(v => beef.processing = v)} unit="per Pound * Dressed Weight"/>
+              <PriceEntry description="Make Patties" currentPrice={beef.patties} onChange={thenSave(v => beef.patties = v)} unit="per Pound"/>
+              <PriceEntry description="Split into Halves" currentPrice={beef.halves} onChange={thenSave(v => beef.halves = v)} unit="per Half"/>
+              <PriceEntry description="Divide Halves into Quaters" currentPrice={beef.halvesToQuaters} onChange={thenSave(v => beef.halvesToQuaters = v)} unit="per Quarter"/>
+              <PriceEntry description="Extra Boning" currentPrice={beef.extraBoning} onChange={thenSave(v => beef.extraBoning = v)} unit="per Quarter"/>
+              <PriceEntry description="Cut Stew Meat" currentPrice={beef.cutStewMeat} onChange={thenSave(v => beef.cutStewMeat = v)} unit="per Pound"/>
+              <PriceEntry description="Bone and Tenderize Round Steaks" currentPrice={beef.boneAndTenderizeRoundSteaks} onChange={thenSave(v => beef.boneAndTenderizeRoundSteaks = v)} unit="per Pound"/>
+              <PriceEntry description="Make Cubed Steaks" currentPrice={beef.makeCubedSteaks} onChange={thenSave(v => beef.makeCubedSteaks = v)} unit="per Half"/>
+              <PriceEntry description="Bone Or Prime Rib" currentPrice={beef.boneOrPrimeRib} onChange={thenSave(v => beef.boneOrPrimeRib = v)} unit="per Half"/>
+              <PriceEntry description="Bone out Loin" currentPrice={beef.boneOutLoin} onChange={thenSave(v => beef.boneOutLoin = v)} unit="per Half"/>
             </tbody>
           </table>
 
@@ -44,23 +68,72 @@ export const PriceSheet = () => {
               </tr>
             </thead>
             <tbody className="border-b border-tomato-600">
-              <PriceEntry description="Slaughter Fee < 150lbs" currentPrice={50.00} unit="per Animal"/>
-              <PriceEntry description="Slaughter Fee > 150lbs" currentPrice={60.00} unit="per Animal"/>
-              <PriceEntry description="Processing" currentPrice={1.00} unit="Per Pound * Dressed Weight"/>
-              <PriceEntry description="Cure" currentPrice={1.00} unit="Per Pound of Cured Meat"/>
-              <PriceEntry description="Sausage" currentPrice={0.25} unit="per Pound of Sausage"/>
+              <PriceEntry description="Slaughter Fee < 150lbs" currentPrice={pork.slaughter} onChange={thenSave(v => pork.slaughter = v)} unit="per Animal"/>
+              <PriceEntry description="Slaughter Fee > 150lbs" currentPrice={pork.slaughter150lb} onChange={thenSave(v => pork.slaughter150lb = v)} unit="per Animal"/>
+              <PriceEntry description="Processing" currentPrice={pork.processing} onChange={thenSave(v => pork.processing = v)} unit="Per Pound * Dressed Weight"/>
+              <PriceEntry description="Cure" currentPrice={pork.cure} onChange={thenSave(v => pork.cure = v)} unit="Per Pound of Cured Meat"/>
+              <PriceEntry description="Sausage" currentPrice={pork.sausage} onChange={thenSave(v => pork.sausage = v)} unit="per Pound of Sausage"/>
             </tbody>
           </table>
         </div>
-      </div>
-    )
+      </GlobalEdit.Provider>
+    </div>
+  )
 }
 
-const PriceEntry = ({ description, currentPrice, unit }: {description: String, currentPrice: number, unit: String}) => {
-    return (
+const PriceEntry = ({ description, currentPrice, onChange, unit }: {description: string, currentPrice: number, onChange: (val:number)=>void,unit: string}) => {
+  const [editing, setEditing] = useState(false)
+  const [displayValue, setDisplayValue] = useState(currentPrice.toFixed(2))
+  const [value, setValue] = useState(currentPrice.toFixed(2))
+  const globalEdit = useContext(GlobalEdit)
+  const displayRef = useRef<HTMLSpanElement>()
+  const inputRef = useRef<HTMLInputElement>()
+
+  const isEdit = globalEdit || editing
+
+  if(globalEdit && editing) {
+    setEditing(false)
+  }
+
+  const doChange = (s: string) => {
+    setValue(s)
+    const v = parseFloat(s)
+    if(!isNaN(v)) {
+      onChange(v)
+      setDisplayValue(v.toFixed(2))
+    }
+  }
+
+  if(displayRef.current !== undefined) {
+    const w = displayRef.current.getBoundingClientRect().width
+    if(w !== 0) {
+      inputRef.current.style.width = w+'px'
+    }
+  }
+
+  return (
       <tr>
         <td className="border-t border-red-600 pl-2 pr-4">{description}</td>
-        <td className="border-t border-red-600 w-20 text-right pr-4">${currentPrice.toFixed(2)}</td>
+        <td className="border-t border-red-600 w-20 text-right pr-4">
+          $
+          <span ref={displayRef} className={isEdit?'hidden':''} onDoubleClick={e => {
+            if(globalEdit) {
+              return
+            }
+            inputRef.current.classList.remove('hidden') //Can't be hidden to focus
+            inputRef.current.focus()
+            setEditing(true)
+            e.preventDefault()
+            e.stopPropagation()
+          }}>{displayValue}</span>
+          <input 
+            ref={inputRef}
+            className={(isEdit?'':'hidden ') + " bg-red-100"}
+            value={value} 
+            onChange={e => doChange(e.target.value)} 
+            onBlur={() => setEditing(false)} 
+          />
+        </td>
         <td className="border-t border-red-600 px-4">{unit}</td>
       </tr>
     )

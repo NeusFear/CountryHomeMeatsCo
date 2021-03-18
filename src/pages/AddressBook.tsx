6e9de000f6e9b19425116b6dd2379c2d86bf3414
Brandon Davis <neusfear@gmail.com>
2021-03-18 09:08:53 -0500

    Style Address Book page

--- src/pages/AddressBook.tsx
+++ src/pages/AddressBook.tsx
@@ -1,28 +1,45 @@
-import { SvgTrash } from "../assets/Icons";
+import { SvgNewUser, SvgNotes, SvgSearch, SvgTrash } from "../assets/Icons";
 
 export const AddressBook = () => {
   
     return (
-    <div className="w-full h-screen flex flex-col">
-        <div className="flex flex-row w-full h-14 bg-gray-800 pt-1">
-          <div className="text-white text-4xl font-bold ml-4">Address Book</div>
+      <div className="flex flex-col h-full">
+        <div className="bg-gray-800 py-2 px-2 flex flex-row justify-end">
+          <div className="relative rounded-md shadow-sm flex-grow">
+            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+              <span className="text-gray-500 sm:text-sm">
+                <SvgSearch />
+              </span>
+            </div>
+            <input type="text" name="search" className="block w-full pl-9 pr-12 border-gray-300 rounded-md h-10" placeholder="Search" />
+          </div>
+          <div className="transform cursor-pointer px-4 w-12 ml-1 pt-3 hover:bg-tomato-600 border-gray-300 rounded-md h-10 flex-initial bg-tomato-700 text-white"><SvgSearch /></div>
+          <div className="transform cursor-pointer px-4 w-12 ml-1 pt-3 hover:bg-tomato-600 border-gray-300 rounded-md h-10 flex-initial bg-tomato-700 text-white"><SvgNewUser /></div>
         </div>
-        <div className="bg-gray-400 px-1 py-0.5 shadow-sm mb-2">
-          <span className="ml-2 text-gray-700">This page keeps track of all the vendor and insurance phone numbers and things that are needed often.</span>
+        <div className="bg-gray-400 px-1 py-0.5 shadow-sm flex flex-row mb-2">
+          <span className="ml-4 pr-2 flex-1 text-gray-700">Company</span>
+          <span className="pr-2 flex-1 text-gray-700 ml-2">Primary Contact</span>
+          <span className="pr-2 flex-1 text-gray-700 ml-2">Phone Number</span>
+          <span className="pr-2 flex-1 text-gray-700 ml-2">Email</span>
+          <span className="w-24"></span>
         </div>
         <div className="px-4 mt-4 h-full overflow-y-scroll">
-          <PhoneEntry />
+          <AddressBookEntry />
         </div>
-      </div>
+    </div>
     )
 }
 
-const PhoneEntry = () => {
+const AddressBookEntry = () => {
     return (
       <div className="cursor-pointer bg-gray-100 hover:shadow-md rounded-lg px-2 py-2 shadow-sm flex flex-row mb-2">
-        <span className="pr-2 flex-1">Name</span>
+        <span className="pr-2 flex-1">Amazon</span>
+        <span className="pr-2 flex-1 flex flex-row">Jeff Bezos</span>
         <span className="pr-2 flex-1 flex flex-row">(123)456-789</span>
         <span className="pr-2 flex-1 flex flex-row">something@email.com</span>
+        <span className="hover:text-tomato-400 text-gray-600 h-6 w-6 mr-1 flex-shrink pt-1">
+          <SvgNotes />
+        </span>
         <span className="hover:text-tomato-400 text-gray-600 h-6 w-6 mr-1 flex-shrink pt-1">
           <SvgTrash />
         </span>

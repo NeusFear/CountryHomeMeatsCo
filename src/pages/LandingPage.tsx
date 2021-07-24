97cd3b12ced1e8f128e42f483631069067cc81b9
Brandon Davis <neusfear@gmail.com>
2021-07-24 14:40:44 -0500

    Make landing page work better on small screens

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -12,14 +12,13 @@ export const LandingPage = () => {
         <div className="bg-gray-200 h-1/5 rounded-lg">
           No special events today.
         </div>
-        <div className="flex flex-row mt-4 flex-grow">
-          <div className="flex-grow bg-gray-200 h-full rounded-lg">
+        <div className="flex flex-row mt-4 h-4/5">
+          <div className="flex-grow bg-gray-200 rounded-lg">
             <div className="bg-gray-800 font-semibold rounded-t-lg text-white px-2 py-1">Upcoming Events</div>
             <div className="bg-white m-2">
               Some notes go here<br />
               more notes<br />
               something else<br />
-              <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
             </div>
           </div>
           <div className="flex-grow bg-gray-200 mx-4 h-full rounded-lg">
@@ -39,20 +38,17 @@ export const LandingPage = () => {
             </div>
           </div>
           <div className="w-1/5 h-full flex flex-col">
-            <Link to={timeSheet} className="rounded-lg flex-grow text-white text-7xl bg-green-200 hover:bg-green-100 text-center shadow-sm hover:shadow-md border-4 border-green-600">
-              <br />
-              <SvgTimesheets className="w-full ml-1" />
-              <p className="text-xl">Time Sheet</p>
+            <Link to={timeSheet} className="w-full h-1/3 rounded-lg text-white text-7xl bg-green-200 hover:bg-green-100 text-center shadow-sm hover:shadow-md border-4 border-green-600">
+              <SvgTimesheets className="relative w-1/3 h-1/3 ml-1 left-1/2 transform -translate-x-1/2 top-1/2 -translate-y-2/3" />
+              <p className="text-xl relative left-1/2 transform -translate-x-1/2 top-1/2 -translate-y-3/4">Time Sheet</p>
             </Link>
-            <Link to={addressBook} className="rounded-lg my-4 flex-grow text-white text-7xl bg-tomato-200 hover:bg-tomato-100 text-center shadow-sm hover:shadow-md border-4 border-tomato-600">
-              <br /> 
-              <SvgAddressbook className="w-full ml-1" />
-              <p className="text-xl">Address Book</p>
+            <Link to={addressBook} className="w-full h-1/3 rounded-lg my-4 text-white text-7xl bg-tomato-200 hover:bg-tomato-100 text-center shadow-sm hover:shadow-md border-4 border-tomato-600">
+              <SvgAddressbook className="relative w-1/3 h-1/3 ml-1 left-1/2 transform -translate-x-1/2 top-1/2 -translate-y-2/3" />
+              <p className="text-xl relative left-1/2 transform -translate-x-1/2 top-1/2 -translate-y-3/4">Address Book</p>
             </Link>
-            <Link to={priceSheet} className="rounded-lg flex-grow text-white text-7xl bg-blue-200 hover:bg-blue-100 text-center shadow-sm hover:shadow-md border-4 border-blue-600">
-              <br />
-              <SvgPrices className="w-full ml-1" />
-              <p className="text-xl">Price List</p>
+            <Link to={priceSheet} className="w-full h-1/3 rounded-lg text-white text-7xl bg-blue-200 hover:bg-blue-100 text-center shadow-sm hover:shadow-md border-4 border-blue-600">
+              <SvgPrices className="relative w-1/3 h-1/3 ml-1 left-1/2 transform -translate-x-1/2 top-1/2 -translate-y-2/3" />
+              <p className="text-xl relative left-1/2 transform -translate-x-1/2 top-1/2 -translate-y-3/4">Price List</p>
             </Link>
           </div>
         </div>

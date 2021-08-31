b264ce0dcc94296b46beccd6c9ed1aa44aa2924a
Brandon Davis <neusfear@gmail.com>
2021-08-31 10:00:09 -0500

    Add totals and style incooler closes #17

--- src/pages/InCoolerPage.tsx
+++ src/pages/InCoolerPage.tsx
@@ -18,13 +18,13 @@ export const InCoolerPage = () => {
       <div className="flex flex-row w-full h-14 bg-gray-800 pt-1">
         <div className="text-white text-4xl font-bold ml-4">IN THE COOLER</div>
       </div>
-      <div className="flex flex-row">
-        <div className="flex flex-col w-full m-4 bg-gray-100 rounded-md p-4">
-          <div>BEEF:</div>
+      <div className="flex flex-row pt-4">
+        <div className="flex-grow bg-gray-200 mx-4 h-full rounded-lg">
+          <div className="bg-gray-800 font-semibold rounded-t-lg text-white px-2 py-1" >BEEF: {cows.length}</div>
           {cows.map(c => <AnimalInfoEntry key={c.id} animal={c} />)}
         </div>
-        <div className="flex flex-col w-full m-4 bg-gray-100 rounded-md p-4">
-          <div>PORK:</div>
+        <div className="flex-grow bg-gray-200 mx-4 h-full rounded-lg">
+          <div className="bg-gray-800 font-semibold rounded-t-lg text-white px-2 py-1" >PORK: {pigs.length}</div>
           {pigs.map(c => <AnimalInfoEntry key={c.id} animal={c} />)}
         </div>
       </div>
@@ -46,7 +46,7 @@ const AnimalInfoEntry = ({ animal }: { animal: IAnimal }) => {
   }
 
   return (
-    <div className="bg-gray-200 rounded-md p-2 flex flex-row text-xs m-0.5 hover:bg-gray-300" onClick={() => history.push(animalDetailsPage, animal.id)}>
+    <div className="bg-gray-100 rounded-md p-2 flex flex-row text-xs my-1 mx-2 hover:bg-gray-300" onClick={() => history.push(animalDetailsPage, animal.id)}>
       <p>{user.name}</p>
       <p>#{paddedAnimalId(animal)}</p>
     </div>

39ff2b5aaa54e37b571a2cbd740b0ac9598f911b
Wyn Price <wynprice999@gmail.com>
2021-10-17 22:05:35 +0100

    Fix #50

--- src/Util.tsx
+++ src/Util.tsx
@@ -7,7 +7,8 @@ export const normalizeDay = (date: Date = new Date()) =>
 
 export const formatDay = (date: Date) => date === undefined ? "???" : date.getMonth() + "/" + date.getDate() + "/" + date.getFullYear() //America moment
 
-export const formatPhoneNumber = (numStr: String) => {
+export const formatPhoneNumber = (numStr: string) => {
+    numStr = numStr.replace(/-/g, "")
     return "(" + numStr.substr(0, 3) + ") " + numStr.substr(3, 3) + "-" + numStr.substr(6);
 }
 
